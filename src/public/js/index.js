80fbddc27086108adc10d863e75fd3e9aad8a0c5
190101101 <190101101@mail.ru>
2024-08-30 03:47:47 +0400

    update

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,4 @@
+import '/node/axios/dist/axios.js';
 import { client } from './socket.js';
 import { displayUsers, displayMessages } from './boot.js';
 
@@ -6,4 +7,19 @@ window.addEventListener('DOMContentLoaded', () => {
     users.forEach((user) => displayUsers(user));
     messages.forEach((message) => displayMessages(message));
   });
+
+  // burda socket olmalidir
+  const UserFetcher = async () => {
+    const response = await axios.get(`http://localhost:5000/users`);
+    response.data.forEach((data) => displayUsers(data));
+  };
+
+  // UserFetcher();
+
+  const MessageFetcher = async () => {
+    const response = await axios.get(`http://localhost:5000/messages`);
+    response.data.forEach((data) => displayMessages(data));
+  };
+
+  // MessageFetcher();
 });
