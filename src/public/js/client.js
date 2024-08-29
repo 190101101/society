93ed1831f32e6c34ea267c8fdc9c439ff03dbec9
190101101 <190101101@mail.ru>
2024-08-30 03:19:23 +0400

    elements

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,28 +1,34 @@
 import { client } from './socket.js';
 import { displayMessages, displayUsers } from './boot.js';
+import { onlineText, usersText, messagesText } from './elements.js';
+
+// sidebar content
 
 client.on('server:data', (data) => {
-  document.querySelector('.online-text').textContent = data.online;
-  document.querySelector('.users-text').textContent = data.users;
-  document.querySelector('.messages-text').textContent = data.messages;
+  onlineText.textContent = data.online;
+  usersText.textContent = data.users;
+  messagesText.textContent = data.messages;
 });
 
 client.on('server:online', (data) => {
-  document.querySelector('.online-text').textContent = data.online;
+  onlineText.textContent = data.online;
 });
 
 client.on('server:users', (data) => {
-  document.querySelector('.users-text').textContent = data.users;
+  usersText.textContent = data.users;
 });
 
 client.on('server:messages', (data) => {
-  document.querySelector('.messages-text').textContent = data.messages;
+  messagesText.textContent = data.messages;
 });
 
 client.on('server:newuser', (data) => {
   displayUsers(data, 'afterbegin');
 });
 
+// sidebar content
+
+// main content
 client.on('server:message', (data) => {
   console.log(data);
   displayMessages(data);
---
+++ src/public/js/elements.js
@@ -0,0 +1,3 @@
+export const onlineText = document.querySelector('.online-text');
+export const usersText = document.querySelector('.users-text');
+export const messagesText = document.querySelector('.messages-text');
